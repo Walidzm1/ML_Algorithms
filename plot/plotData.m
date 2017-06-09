83ca19d7202e4eba36da96ab5bbd7c706a8e96df
Walid <Walidzm1@users.noreply.github.com>
2017-06-09 19:30:29 +0200

    Create plotData.m

--- plot/plotData.m
+++ plot/plotData.m
@@ -1,7 +1,6 @@
 function plotData(x, y)
 %PLOTDATA Plots the data points x and y into a new figure 
 figure; % open a new figure window
-
-  plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
 
 end
